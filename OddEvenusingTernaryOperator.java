9a397e2f403bc7ff227953b25654647ed988af63
Kushal1184 <131439305+Kushal1184@users.noreply.github.com>
2025-06-20 13:10:49 +0530

    Update OddEvenusingTernaryOperator.java

--- OddEvenusingTernaryOperator.java
+++ OddEvenusingTernaryOperator.java
@@ -5,6 +5,7 @@ public class OddEvenusingTernaryOperator {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Value: ");
         int n= sc.nextInt();
+        //TernaryOperator
         String large=((n%2==0)?"This Number is Even":"This Number is Odd");
         System.out.println(large);
 
