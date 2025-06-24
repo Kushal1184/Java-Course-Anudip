4fb8facec23ed78931776677068f1cac61396893
Kushal1184 <131439305+Kushal1184@users.noreply.github.com>
2025-06-24 19:32:01 +0530

    Add files via upload

---
+++ righttrianglepattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class righttrianglepattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value:");
+        int n= sc.nextInt();
+
+        for (int i=0;i<n;i++){
+            for (int s = 0; s < n - i - 1; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
