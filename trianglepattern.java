0335b8a1d398942adf18620aeeb3faf0f50111d3
Kushal1184 <131439305+Kushal1184@users.noreply.github.com>
2025-06-24 13:15:52 +0530

    Update trianglepattern.java

--- trianglepattern.java
+++ trianglepattern.java
@@ -6,8 +6,8 @@ public class trianglepattern {
         System.out.print("Enter the value:");
         int n= sc.nextInt();
 
-        for (int i=0;i<n;i++){
-            for (int j = 0; j <= i; j++) {
+        for (int i=1;i<=n;i++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
