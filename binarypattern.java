4c0f9b141243ecc8b4d2fa71fbd75c9b8d3b27eb
Kushal1184 <131439305+Kushal1184@users.noreply.github.com>
2025-06-25 12:37:19 +0530

    Update binarypattern.java

--- binarypattern.java
+++ binarypattern.java
@@ -5,12 +5,15 @@ public class binarypattern {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the value:");
         int n= sc.nextInt();
-        int a=1;
 
         for (int i=0;i<n;i++){
             for (int j = 0; j <= i; j++) {
-                System.out.print(a % 2);
-                a++;
+                int sum=i+j;
+                if (sum%2==0) {
+                    System.out.print(1);
+                }else {
+                    System.out.print(0);
+                }
             }
             System.out.println();
         }
