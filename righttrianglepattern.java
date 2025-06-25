50d35acf6201aeda296113cdb689b3ee8af0d152
Kushal1184 <131439305+Kushal1184@users.noreply.github.com>
2025-06-25 13:17:11 +0530

    Update righttrianglepattern.java

--- righttrianglepattern.java
+++ righttrianglepattern.java
@@ -6,11 +6,11 @@ public class righttrianglepattern {
         System.out.print("Enter the value:");
         int n= sc.nextInt();
 
-        for (int i=0;i<n;i++){
-            for (int s = 0; s < n - i - 1; s++) {
+        for (int i=1;i<=n;i++){
+            for (int s = 0; s < n -i; s++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
